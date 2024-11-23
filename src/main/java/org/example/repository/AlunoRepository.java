1880214e1890230efd75aeed3f7f3f5a4cd43a9e
efraimff <emfs6@aluno.ifal.edu.br>
2024-11-23 16:45:36 -0300

    feat: add email search functionality to Aluno and Instrutor repositories
    - Added 'findByEmail' method to AlunoRepository for searching by email.
    - Added  method to InstrutorRepository for searching by email.

--- src/main/java/org/example/repository/AlunoRepository.java
+++ src/main/java/org/example/repository/AlunoRepository.java
@@ -4,10 +4,12 @@ import org.example.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AlunoRepository extends JpaRepository<Aluno, UUID> {
     List<Aluno> findByNomeContainingIgnoreCaseOrSobrenomeContainingIgnoreCase(String nome, String sobrenome);  // Buscar alunos pelo nome ou sobrenome
     List<Aluno> findByInstrutorId(UUID instrutorId);  // Buscar alunos de um instrutor
     long countByInstrutorId(UUID instrutorId);  // Contar alunos de um instrutor
+    Optional<Aluno> findByEmail(String email); // Buscar aluno pelo email
 }
--- src/main/java/org/example/repository/InstrutorRepository.java
+++ src/main/java/org/example/repository/InstrutorRepository.java
@@ -4,8 +4,10 @@ import org.example.model.Instrutor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface InstrutorRepository extends JpaRepository<Instrutor, UUID> {
     List<Instrutor> findByNomeContainingIgnoreCaseAndSobrenomeContainingIgnoreCase(String nome, String sobrenome);  // Buscar instrutores pelo nome e sobrenome
+    Optional<Instrutor> findByEmail(String email); // Buscar instrutor pelo email
 }
